827d67d23402508ec83c8b6733774749c5214590
longdhp <longdhp@di4l.vn>
2022-07-06 15:22:02 +0700

    chore: enable auto play

--- lib/video_player_web.dart
+++ lib/video_player_web.dart
@@ -82,7 +82,7 @@ class VideoPlayerPlugin extends VideoPlayerPlatform {
 
       // Allows Safari iOS to play the video inline
     videoElement.setAttribute('playsinline', 'true');
-    videoElement.setAttribute('autoplay', 'false');
+    videoElement.setAttribute('autoplay', 'true');
 
     // TODO(hterkelsen): Use initialization parameters once they are available
     ui.platformViewRegistry.registerViewFactory(
